e38933fa55a40251cef0eadd5c91537d555f7c43
katievlasic <kathrynvlasic@gmail.com>
2023-05-11 19:14:00 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleboo
   useUnifiedTopology: true
 });
 
+// if in development use 'mongodb://127.0.0.1:27017/googlebooks'
+
 module.exports = mongoose.connection;
